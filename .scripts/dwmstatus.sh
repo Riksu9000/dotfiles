910f0a8693fb99baf0ab65f6c54daf508350b47e
Riku Isokoski <riku@arch-riku.localdomain>
2019-06-16 14:01:39 +0300

    Modularise dwmstatus.sh

--- .scripts/dwmstatus.sh
+++ .scripts/dwmstatus.sh
@@ -1,18 +1,64 @@
 #!/bin/sh
 
+# Separator put in between modules
+SEP="  "
+
 # Update the status bar
 update() {
-	xsetroot -name "$(amixer sget Master | awk '/Front Left:/ {print substr($5,2)}' | sed 's/%.*//')%\
-  $(date '+%a %e.%b %H:%M')"
+	# Clear statusbar of old data
+	BAR=""
+
+	# Status bar is constructed by executing functions
+	song
+	vol
+	clock
+
+	# Write the string to the bar
+	xsetroot -name "$BAR"
+}
+
+backlight() {
+	BAR=$BAR$SEP$(cat /sys/class/backlight/*/actual_brightness)
+}
+
+bat() {
+	BAR=$BAR$SEP$(cat /sys/class/power_supply/BAT0/capacity)
+}
+
+vol() {
+	BAR=$BAR$SEP"VOL "$(amixer sget Master | awk '/Front Left:/ {print substr($5,2)}' | sed 's/%.*//')"%"
+}
+
+clock() {
+	BAR=$BAR$SEP$(date '+%a %e.%b %H:%M')
+}
+
+song() {
+	case "$(playerctl status)" in
+		"Paused")
+			ICON="⏸"
+			;;
+		"Playing")
+			ICON="▶"
+			;;
+		*)
+			ICON=""
+			return
+			;;
+	esac
+	BAR=$BAR$SEP$ICON" "$(playerctl metadata -f '{{title}}')
 }
 
 # update the status bar when this script is signaled with SIGPIPE
 trap update PIPE
 
-# Loop to update the status bar every 30 seconds while also accepting signals
+# Wait for dwm to start
+sleep 1
+
 while true
 do
 	update
+	# Sleep and wait so bar can be updated with SIGPIPE even when sleeping
 	sleep 30 &
 	wait $!
 done
