1bb2af3e7ad4641b5a14f0eb096ce6bd0285ab37
Riku Isokoski <riku@arch-riku.localdomain>
2019-08-03 10:16:07 +0300

    Added simple mounting scripts

---
+++ .scripts/mounter.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FONT="xos4 Terminus:pixelsize=18"
+BLACK="#282828"; GRAY="#928374"; WHITE="#ebdbb2"; SEL="#d65d0e"
+
+systemd-mount $(lsblk -npro "NAME,LABEL,SIZE,TYPE,RM,MOUNTPOINT" | \
+	sed '/part 1 $/!d; s/part 1//' | \
+	dmenu -i -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Mount which drive?" | \
+	awk '{print $1}')
