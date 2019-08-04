02d4d6a0651764b7586e982d16ba953c22554e50
Riku Isokoski <riksu9000@gmail.com>
2019-08-04 15:58:14 +0300

    Vertical mount menus

--- .scripts/mounter.sh
+++ .scripts/mounter.sh
@@ -5,5 +5,5 @@ BLACK="#282828"; GRAY="#928374"; WHITE="#ebdbb2"; SEL="#d65d0e"
 
 systemd-mount $(lsblk -npro "NAME,LABEL,SIZE,TYPE,RM,MOUNTPOINT" | \
 	sed '/part 1 $/!d; s/part 1//' | \
-	dmenu -i -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Mount which drive?" | \
+	dmenu -i -l 5 -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Mount which drive?" | \
 	awk '{print $1}')
