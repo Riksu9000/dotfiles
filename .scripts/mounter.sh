678f6db76fc920736276b84000277a535350d0fb
Riku Isokoski <riksu9000@gmail.com>
2019-08-05 13:40:41 +0300

    Made mounting scripts more robust
    Also fixed mounting of disks without partitions or labels

--- .scripts/mounter.sh
+++ .scripts/mounter.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
 FONT="xos4 Terminus:pixelsize=18"
-BLACK="#282828"; GRAY="#928374"; WHITE="#ebdbb2"; SEL="#d65d0e"
+BLACK=#282828; GRAY=#928374; WHITE=#ebdbb2; SEL=#d65d0e
 
-systemd-mount $(lsblk -npro "NAME,LABEL,SIZE,TYPE,RM,MOUNTPOINT" | \
-	sed '/part 1 $/!d; s/part 1//' | \
-	dmenu -i -l 5 -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Mount which drive?" | \
+# Look for all mountable drives
+DRIVES=$(lsblk -npro "NAME,TYPE,PTTYPE,RM,MOUNTPOINT" | sed '/ 1 /!d; /disk [a-z]/d' | awk '{print $1}')
+
+# Show a menu of mountable drives and put the full path to the variable $CHOSEN
+CHOSEN=$(lsblk $DRIVES -npro "NAME,LABEL,SIZE" | \
+	dmenu -i -l 5 -fn "$FONT" -nb $BLACK -nf $GRAY -sb $SEL -sf $WHITE -p "Mount which drive?" | \
 	awk '{print $1}')
+
+# Get the label for the partition
+LABEL=$(lsblk $CHOSEN -npro "LABEL")
+
+# If there is no label, get UUID
+[ -z $LABEL ] && LABEL=$(lsblk $CHOSEN -npro "UUID")
+
+# Make a directory for mounting the drive
+mkdir -p $HOME/mounts/$LABEL
+
+sudo -A mount $CHOSEN $HOME/mounts/$LABEL && notify-send "Successfully mounted $LABEL" || notify-send "There was an error mounting drive $LABEL"
+
--- .scripts/umounter.sh
+++ .scripts/umounter.sh
@@ -1,9 +1,44 @@
 #!/bin/sh
 
 FONT="xos4 Terminus:pixelsize=18"
-BLACK="#282828"; GRAY="#928374"; WHITE="#ebdbb2"; SEL="#d65d0e"
+BLACK=#282828; GRAY=#928374; WHITE=#ebdbb2; SEL=#d65d0e
 
-systemd-umount $(lsblk -npro "NAME,LABEL,SIZE,TYPE,RM,MOUNTPOINT" | \
-	sed '/part 1 \//!d; s/part 1//' | \
-	dmenu -i -l 5 -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Unmount which drive?" | \
+# Look for all mountable drives
+DRIVES=$(lsblk -npro "NAME,RM,MOUNTPOINT" | sed '/ 1 \//!d' | awk '{print $1}')
+
+[ -z $DRIVES ] && notify-send "No drives to unmount" && exit
+
+# Show a menu of unmountable drives and put the full path to the variable $CHOSEN
+CHOSEN=$(lsblk $DRIVES -npro "NAME,LABEL,SIZE" | \
+	dmenu -i -l 5 -fn "$FONT" -nb $BLACK -nf $GRAY -sb $SEL -sf $WHITE -p "Umount which drive?" | \
 	awk '{print $1}')
+
+# Get the label for the partition
+LABEL=$(lsblk $CHOSEN -npro "LABEL")
+
+# If there is no label, get UUID
+[ -z $LABEL ] && LABEL=$(lsblk $CHOSEN -npro "UUID")
+
+sudo -A umount $CHOSEN || error mount
+
+# Remove the mounting directory
+rm -r $HOME/mounts/$LABEL || error rem
+
+notify-send "Successfully unmounted $LABEL" && exit 0
+
+error() {
+	case $1 in
+		"mount")
+			notify-send "There was an error unmounting drive $LABEL"
+			exit 1
+			;;
+		"rem")
+			notify-send "Couldn\'t remove the mounting directory. Remove $HOME/mounts/$LABEL manually."
+			exit 1
+			;;
+		*)
+			notify-send "An unknown error occurred"
+			exit 1
+			;;
+	esac
+}
