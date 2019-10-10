77b1581eef9cc0dfae6e0a35b734a6b538642d1d
Riku Isokoski <riksu9000@gmail.com>
2019-10-10 13:45:18 +0300

    Even more fixes

--- .local/bin/mounter.sh
+++ .local/bin/mounter.sh
@@ -3,6 +3,11 @@
 FONT="xos4 Terminus:pixelsize=18"
 BLACK=#282828; GRAY=#928374; WHITE=#ebdbb2; SEL=#d65d0e
 
+
+err() {
+	notify-send "There was an error mounting drive $LABEL"
+}
+
 # Look for all mountable drives
 DRIVES=$(lsblk -npro "NAME,TYPE,PTTYPE,RM,MOUNTPOINT" | sed '/ 1 $/!d; /disk [a-z]/d' | awk '{print $1}')
 
@@ -16,7 +21,10 @@ CHOSEN=$(lsblk $DRIVES -npro "NAME,LABEL,SIZE" | \
 [ -z $CHOSEN ] && exit 0
 
 # Get the label for the partition
-LABEL=$(lsblk $CHOSEN -npro "LABEL")
+LABEL=$(lsblk $CHOSEN -no "LABEL")
+
+# Get the partition type
+TYPE=$(lsblk $CHOSEN -no "FSTYPE")
 
 # If there is no label, get UUID
 [ -z $LABEL ] && LABEL=$(lsblk $CHOSEN -npro "UUID")
@@ -24,5 +32,12 @@ LABEL=$(lsblk $CHOSEN -npro "LABEL")
 # Make a directory for mounting the drive
 mkdir -p $HOME/mounts/$LABEL
 
-sudo -A mount -w $CHOSEN $HOME/mounts/$LABEL && notify-send "Successfully mounted $LABEL" || notify-send "There was an error mounting drive $LABEL"
-sudo -A chown "$USER" $HOME/mounts/$LABEL
+if [ "$TYPE" == "vfat" ]
+then
+	sudo -A mount -o uid=$UID -w $CHOSEN $HOME/mounts/$LABEL || err
+else
+	sudo -A mount -w $CHOSEN $HOME/mounts/$LABEL || err
+	sudo -A chown "$USER" $HOME/mounts/$LABEL
+fi
+
+notify-send "Successfully mounted $LABEL"
