55afee1b7953305b69a77b7c26a8d813021ab972
Riku Isokoski <riksu9000@gmail.com>
2019-11-13 10:33:00 +0200

    Added support for external HDDs and shellchecked

--- .local/bin/mounter.sh
+++ .local/bin/mounter.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 FONT="xos4 Terminus:pixelsize=18"
 BLACK=#282828; GRAY=#928374; WHITE=#ebdbb2; SEL=#d65d0e
@@ -9,35 +9,36 @@ err() {
 }
 
 # Look for all mountable drives
-DRIVES=$(lsblk -npro "NAME,TYPE,PTTYPE,RM,MOUNTPOINT" | sed '/ 1 $/!d; /disk [a-z]/d' | awk '{print $1}')
+DRIVES=$(lsblk -npro "NAME,TYPE,PTTYPE,RM,MOUNTPOINT" | sed '/ [0-9] $/!d; /disk [a-z]/d' | awk '{print $1}')
 
-[ -z $DRIVES ] && notify-send "No mountable drives detected" && exit 0
+[ -z "$DRIVES" ] && notify-send "No mountable drives detected" && exit 0
 
 # Show a menu of mountable drives and put the full path to the variable $CHOSEN
-CHOSEN=$(lsblk $DRIVES -npro "NAME,LABEL,SIZE" | \
-	dmenu -i -l 5 -fn "$FONT" -nb $BLACK -nf $GRAY -sb $SEL -sf $WHITE -p "Mount which drive?" | \
+CHOSEN=$(lsblk "$DRIVES" -npro "NAME,LABEL,SIZE" | \
+	dmenu -i -l 5 -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Mount which drive?" | \
 	awk '{print $1}')
 
-[ -z $CHOSEN ] && exit 0
+[ -z "$CHOSEN" ] && exit 0
 
 # Get the label for the partition
-LABEL=$(lsblk $CHOSEN -no "LABEL")
+LABEL=$(lsblk "$CHOSEN" -no "LABEL")
 
 # Get the partition type
-TYPE=$(lsblk $CHOSEN -no "FSTYPE")
+TYPE=$(lsblk "$CHOSEN" -no "FSTYPE")
 
 # If there is no label, get UUID
-[ -z $LABEL ] && LABEL=$(lsblk $CHOSEN -npro "UUID")
+[ -z "$LABEL" ] && LABEL=$(lsblk "$CHOSEN" -no "UUID")
 
 # Make a directory for mounting the drive
-mkdir -p $HOME/mounts/$LABEL
+mkdir -p "$HOME/mounts/$LABEL"
 
-if [ "$TYPE" == "vfat" ]
+if [ "$TYPE" = "vfat" ]
 then
-	sudo -A mount -o uid=$UID -w $CHOSEN $HOME/mounts/$LABEL || err
+	# In POSIX sh, UID is undefined
+	sudo -A mount -o uid="$UID" -w "$CHOSEN" "$HOME/mounts/$LABEL" || err
 else
-	sudo -A mount -w $CHOSEN $HOME/mounts/$LABEL || err
-	sudo -A chown "$USER" $HOME/mounts/$LABEL
+	sudo -A mount -w "$CHOSEN" "$HOME/mounts/$LABEL" || err
+	sudo -A chown "$USER" "$HOME/mounts/$LABEL"
 fi
 
 notify-send "Successfully mounted $LABEL"
--- .local/bin/umounter.sh
+++ .local/bin/umounter.sh
@@ -4,27 +4,28 @@ FONT="xos4 Terminus:pixelsize=18"
 BLACK=#282828; GRAY=#928374; WHITE=#ebdbb2; SEL=#d65d0e
 
 # Look for all mountable drives
-DRIVES=$(lsblk -npro "NAME,RM,MOUNTPOINT" | sed '/ 1 \//!d' | awk '{print $1}')
+# TODO: Separate external and internal HDDs somehow?
+DRIVES=$(lsblk -npro "NAME,RM,MOUNTPOINT" | sed '/ [0-9] \//!d' | awk '{print $1}')
 
-[ -z $DRIVES ] && notify-send "No drives to unmount" && exit
+[ -z "$DRIVES" ] && notify-send "No drives to unmount" && exit
 
 # Show a menu of unmountable drives and put the full path to the variable $CHOSEN
-CHOSEN=$(lsblk $DRIVES -npro "NAME,LABEL,SIZE" | \
-	dmenu -i -l 5 -fn "$FONT" -nb $BLACK -nf $GRAY -sb $SEL -sf $WHITE -p "Umount which drive?" | \
+CHOSEN=$(lsblk "$DRIVES" -npro "NAME,LABEL,SIZE" | \
+	dmenu -i -l 5 -fn "$FONT" -nb "$BLACK" -nf "$GRAY" -sb "$SEL" -sf "$WHITE" -p "Umount which drive?" | \
 	awk '{print $1}')
 
-[ -z $CHOSEN ] && exit
+[ -z "$CHOSEN" ] && exit
 
 # Get the label for the partition
-LABEL=$(lsblk $CHOSEN -npro "LABEL")
+LABEL=$(lsblk "$CHOSEN" -npro "LABEL")
 
 # If there is no label, get UUID
-[ -z $LABEL ] && LABEL=$(lsblk $CHOSEN -npro "UUID")
+[ -z "$LABEL" ] && LABEL=$(lsblk "$CHOSEN" -npro "UUID")
 
-sudo -A umount $CHOSEN || error mount
+sudo -A umount "$CHOSEN" || error mount
 
 # Remove the mounting directory
-rm -r $HOME/mounts/$LABEL || error rem
+rm -r "$HOME/mounts/$LABEL" || error rem
 
 notify-send "Successfully unmounted $LABEL" && exit 0
 
