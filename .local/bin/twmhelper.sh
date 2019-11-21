566dd2b947680d7f164b1a6577ee5e734e54033d
Riku Isokoski <riksu9000@gmail.com>
2019-11-21 15:47:09 +0200

    Add twmhelper.sh

---
+++ .local/bin/twmhelper.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+vol() {
+	VOL=$(amixer sget Master | awk '/Front Left:/ {print $5 $6}' | sed -e 's/\]\[off\]/ \[muted\]/; s/\]\[on\]//; s/^\[//')
+	notify-send -t 1000 "Volume: $VOL" -h string:x-canonical-private-synchronous:volumechange
+}
+
+case $1 in
+	volup)
+		amixer sset Master 5%+
+		vol
+		;;
+	voldown)
+		amixer sset Master 5%-
+		vol
+		;;
+	volmute)
+		amixer sset Master toggle
+		vol
+		;;
+	blup)
+		light -A 4
+		;;
+	bldown)
+		light -U 4
+		;;
+	playnext)
+		playerctl next
+		;;
+	playprev)
+		playerctl prev
+		;;
+	playpause)
+		playerctl play-pause
+		;;
+	screenshot)
+		FILENAME=$(scrot '%F_%H%M%S.png' -e 'echo $f')
+		xclip -selection clipboard -t image/png -i "$HOME/$FILENAME"
+		notify-send "Screenshot saved to file $FILENAME"
+esac
+
