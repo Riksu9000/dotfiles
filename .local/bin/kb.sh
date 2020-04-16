b7f402f3e719ddd3fa283996417ce61b25b1231e
Riku Isokoski <riksu9000@gmail.com>
2020-04-17 00:02:56 +0300

    Update keyboard configs and fix reapplying options

--- .local/bin/kb.sh
+++ .local/bin/kb.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Clear previous options
+setxkbmap -option
+
 setxkbmap -layout us,fi
-setxkbmap -option grp:alt_caps_toggle
+#setxkbmap -option grp:alt_caps_toggle
+setxkbmap -option grp:shifts_toggle
 xset r rate 200 30
