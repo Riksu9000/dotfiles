2cbc312742434e5dbbcf862a7b20d5a6d7e925c0
Riku Isokoski <riksu9000@gmail.com>
2020-05-12 21:03:44 +0300

    Fix pac.sh

--- .local/bin/blocks/pac.sh
+++ .local/bin/blocks/pac.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-refresh() {
-	sudo pacman -Sy > /dev/null
-	pkill -RTMIN+4 dwmblocks
-}
+SIGNAL=10
 
-download() {
-	[ -n "$UPDATABLE" ] && setsid "$TERMINAL" -e sudo pacman -Suw
-	pkill -RTMIN+4 dwmblocks
+# TODO: Is there a simple way to update output in between running commands?
+
+refresh() {
+	sudo -A pacman -Sy > /dev/null
 }
 
 liveupdate() {
-	PACKAGES=$(printf "%s" "$UPDATABLE" | sed '/linux/d;/nvidia/d;/\n/d')
+	refresh
+	PACKAGES=$(pacman -Qu | sed '/\[ignored\]/d;/linux/d;/nvidia/d;/\n/d;s/ .*//')
 	[ -n "$PACKAGES" ] && setsid "$TERMINAL" -e sudo pacman -S --noconfirm $PACKAGES
-	pkill -RTMIN+4 dwmblocks
 }
+
+case $BLOCK_BUTTON in
+	1) refresh ;;
+	2) setsid "$TERMINAL" -e sudo pacman -Syu ;;
+	3) liveupdate ;;
+esac
 
 UPDATABLE=$(pacman -Qu | sed '/\[ignored\]/d;s/ .*//')
 if [ -n "$UPDATABLE" ]
@@ -22,14 +26,6 @@ then
 	LIVECOUNT=$(echo "$UPDATABLE" | sed '/\[ignored\]/d;/linux/d;/nvidia/d;/\n/d' | wc -l)
 	FULLCOUNT=$(echo "$UPDATABLE" | wc -l)
 fi
-
-case $BLOCK_BUTTON in
-	1) printf "%s" "♻"
-	   refresh & ;;
-	#1) printf "%s" "♻" && sudo -A pacman -Sy > /dev/null && pkill -RTMIN+4 dwmblocks & ;;
-	2) setsid "$TERMINAL" -e sudo pacman -Syu ;;
-	3) liveupdate ;;
-esac
 
 if [ -n "$FULLCOUNT" ] && [ "$FULLCOUNT" -gt 0 ]
 then
