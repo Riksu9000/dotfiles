7eb815e53ee082b0adcdcfa863b9e7739e397b41
Riku Isokoski <riksu9000@gmail.com>
2020-05-13 13:28:29 +0300

    Adjust margins and padding

--- .local/bin/emoji.sh
+++ .local/bin/emoji.sh
@@ -15,7 +15,7 @@ FONT="xos4 Terminus 18"
 
 # Linepadding needs to be set depending on the font so the emoji won't get
 # clipped vertically
-LINEPADDING=4
+LINEPADDING=8
 
 BG="#282828"; FG="#a89984"; HLBG="#d65d0e"; HLFG="#ebdbb2"
 #BG="#000000"; FG="#bbbbbb"; HLBG="#005577"; HLFG="#eeeeee"
@@ -23,12 +23,14 @@ BG="#282828"; FG="#a89984"; HLBG="#d65d0e"; HLFG="#ebdbb2"
 BORDERCOLOR="#504945"
 BW=4
 PADDING=0
+LINEMARGIN=0
 
 # Show a list of emoji and awk the selected emoji to variable $SYMBOL
 SYMBOL=$(rofi -dmenu -font "$FONT" -separator-style none -columns 1 \
 -bw "$BW" \
 -color-normal "0,$FG,0,$HLBG,$HLFG" \
 -color-window "$BG, $BORDERCOLOR, $HLBG" \
+-line-margin "$LINEMARGIN" \
 -line-padding "$LINEPADDING" \
 -lines "$LINES" \
 -p "Copy emoji to clipboard" \
