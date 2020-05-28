7fb6ebb46d35d94b69e0798d4768b3da8ddb0051
Riku Isokoski <riksu9000@gmail.com>
2020-05-28 12:00:38 +0300

    Update pac.sh

--- .local/bin/blocks/pac.sh
+++ .local/bin/blocks/pac.sh
@@ -5,12 +5,15 @@ SIGNAL=10
 # TODO: Is there a simple way to update output in between running commands?
 
 refresh() {
+	notify-send -t 2000 -h string:x-canonical-private-synchronous:updatecheck "Checking for updates"
 	sudo -A pacman -Sy > /dev/null
+	notify-send -t 1 -h string:x-canonical-private-synchronous:updatecheck " "
 }
 
+# TODO: This is probably a bad practice. Find a better solution
 liveupdate() {
 	refresh
-	PACKAGES=$(pacman -Qu | sed '/\[ignored\]/d;/linux/d;/nvidia/d;/\n/d;s/ .*//')
+	PACKAGES=$(pacman -Qu | sed '/\[ignored\]/d;/^linux/d;/^nvidia/d;/^btrfs-progs/d;/.*-dkms/d;/\n/d;s/\ .*//')
 	[ -n "$PACKAGES" ] && setsid "$TERMINAL" -e sudo pacman -S --noconfirm $PACKAGES
 }
 
@@ -20,17 +23,13 @@ case $BLOCK_BUTTON in
 	3) liveupdate ;;
 esac
 
-UPDATABLE=$(pacman -Qu | sed '/\[ignored\]/d;s/ .*//')
+UPDATABLE=$(pacman -Qu | sed '/\[ignored\]/d')
 if [ -n "$UPDATABLE" ]
 then
-	LIVECOUNT=$(echo "$UPDATABLE" | sed '/\[ignored\]/d;/linux/d;/nvidia/d;/\n/d' | wc -l)
+	LIVECOUNT=$(echo "$UPDATABLE" | sed '/linux/d;/nvidia/d;/^btrfs-progs/d;/.*-dkms/d;/\n/d' | wc -l)
 	FULLCOUNT=$(echo "$UPDATABLE" | wc -l)
-fi
-
-if [ -n "$FULLCOUNT" ] && [ "$FULLCOUNT" -gt 0 ]
-then
-	printf "%s" "📦$LIVECOUNT"
-	[ "$FULLCOUNT" -gt "$LIVECOUNT" ] && printf "%s" "/$FULLCOUNT Restart required"
+	printf "%s" "📦$FULLCOUNT"
+	[ "$FULLCOUNT" -gt "$LIVECOUNT" ] && printf "%s" " Restart required"
 else
 	echo
 fi
