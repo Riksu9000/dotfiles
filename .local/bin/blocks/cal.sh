0a7c0a4f39ee57a92b07dbd14186e5db46c28960
Riku Isokoski <riksu9000@gmail.com>
2020-06-25 19:19:15 +0300

    Update calendar block

--- .local/bin/blocks/cal.sh
+++ .local/bin/blocks/cal.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 case $BLOCK_BUTTON in
-	1) notify-send "$(cal -w)" ;;
-	3) notify-send "$(cal -w3)" ;;
+	1) notify-send "Calendar" "$(cal --color=always | sed 's/\x1b\[7m/<b>/;s/\x1b\[27m/<\/b>/')" ;;
+	3) notify-send "Calendar" "$(cal -3 --color=always | sed 's/\x1b\[7m/<b>/;s/\x1b\[27m/<\/b>/')" ;;
 esac
 
-date '+%a %e.%b %H:%M'
+date '+%-e.%-m.%Y %H.%M'
+#date '+%F %R'
 
