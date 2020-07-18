958be8a50494da50b56bb3d1749c89154bf45c64
Riku Isokoski <riksu9000@gmail.com>
2020-07-18 14:26:14 +0300

    Fix sng.sh

--- .local/bin/blocks/sng.sh
+++ .local/bin/blocks/sng.sh
@@ -7,9 +7,9 @@ case $BLOCK_BUTTON in
 	5) playerctl volume 0.05- ;;
 esac
 
-[ -z $(command -v playerctl) ] && echo "🎵Install playerctl" && exit 1
+[ -z "$(command -v playerctl)" ] && echo "🎵Install playerctl" && exit 1
 
 # Sleep because resuming playback might take longer than expected
 sleep 0.01s
 
-playerctl metadata -f '{{status}} {{title}}' | sed 's/Playing/▶/;s/Paused/⏸/;s/Stopped/⏹/'
+playerctl metadata -f '{{status}} {{title}}' 2> /dev/null | sed 's/Playing/▶/;s/Paused/⏸/;s/Stopped/⏹/'
