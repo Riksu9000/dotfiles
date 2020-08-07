d907a75c16fdb8a3dd6e66315e877d28777267c6
Riku Isokoski <riksu9000@gmail.com>
2020-08-07 15:32:33 +0300

    launcher.sh: Add regular run prompt

--- .local/bin/launcher.sh
+++ .local/bin/launcher.sh
@@ -5,5 +5,5 @@ LINES=15
 
 MONITOR=${1:-"-5"}
 
-rofi -modi drun -show drun -show-icons -lines "$LINES" -monitor "$MONITOR" -width "$WIDTH"
+rofi -modi drun,run -show drun -show-icons -lines "$LINES" -monitor "$MONITOR" -width "$WIDTH"
 #dmenu_run -l "$LINES"
