33955f1c03b8cd23a1bf784d389a66fd8e5e8f73
Riku Isokoski <riksu9000@gmail.com>
2020-11-28 21:44:18 +0200

    Update emoji.sh

--- .local/bin/emoji.sh
+++ .local/bin/emoji.sh
@@ -2,10 +2,9 @@
 
 EMOJIFILE="$HOME/.local/bin/emojis"
 
+# Rofi loads the file much faster than dmenu
+# Also dmenu doesn't show emojis with multiple codes correctly
 SYMBOL=$(rofi -dmenu -p ">" < "$EMOJIFILE" | cut -d ' ' -f 1)
+#SYMBOL=$(sed '/U+.*U+/d' < "$EMOJIFILE" | dmenu -l 15 | cut -d ' ' -f 1)
 
-# If nothing was selected
-[ -z "$SYMBOL" ] && exit
-
-# Otherwise send a notification
-printf "%s" "$SYMBOL" | xclip && notify-send "$SYMBOL copied"
+[ -n "$SYMBOL" ] && printf "%s" "$SYMBOL" | xclip -selection CLIPBOARD && notify-send "$SYMBOL copied"
