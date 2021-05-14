293cae384f18444117201bc7a50604e1f29991b0
Riku Isokoski <riksu9000@gmail.com>
2021-05-14 18:48:46 +0300

    Update installer

--- extra/install.sh
+++ extra/install.sh
@@ -1,62 +1,65 @@
 #!/bin/sh
 
+set -e
+
 PACKAGELIST=packages
+
+ask() {
+	while :
+	do
+		printf "\033[1m%s [y/N]\033[0m" "$@"
+		read -r ANSWER
+		case "$ANSWER" in
+		[yY]*) return 0 ;;
+		[nN]*) return 1 ;;
+		"") return 1 ;;
+		*) ;;
+		esac
+	done
+}
 
 err() {
 	echo "$@"
 	exit 1
 }
 
-cd "$(dirname "$0")" || exit 1
-
-set -e
+cd "$(dirname "$0")"
 
 [ "$(id -u)" = "0" ] || err "Run as root"
 ping -c 1 -W 1 archlinux.org > /dev/null || err "Enable internet connection"
 
-printf "This script installs some stuff
-Do you want to continue? [y/N] "
-read -r ANSWER
-[ "$ANSWER" != "y" ] && err "Exiting."
+echo "This script installs some stuff.
+This script should be safe to run on an old installation, but no guarantees."
+ask "Do you want to continue?" || err "Exiting."
 
-unset ANSWER
-printf "Create user? [Y/n] "
-read -r ANSWER
-if [ "$ANSWER" != "n" ]
+if ask "Create user?"
 then
 	printf "Enter username: "
 	read -r USERNAME
 	useradd -m -G storage transmission uucp vboxusers video wheel -s /bin/zsh "$USERNAME"
 	passwd "$USERNAME"
 
-	unset ANSWER
-	printf "Install dotfiles for user? [y/N] "
-	read -r ANSWER
-	[ "$ANSWER" = "y" ] && HOME=/home/$USERNAME ../filecopy.sh
+	ask "Install dotfiles for user?" && HOME=/home/$USERNAME ../filecopy.sh
 fi
 
-unset ANSWER
-printf "Replace sudo with doas? [y/N] "
-read -r ANSWER
-if [ "$ANSWER" = "y" ]
+if ask "Replace sudo with doas?"
 then
 	pacman -Rns --noconfirm sudo
 	ln -s doas /bin/sudo
 fi
 
-printf "The rest of the installation is automatic, so sit back and relax"
+echo "The rest of the installation is automatic, so sit back and relax"
 sleep 2
 
-xargs pacman --needed --noconfirm -S < "$PACKAGELIST"
+grep -v '^#' "$PACKAGELIST" | cut -d ' ' -f 1 | xargs pacman --needed --noconfirm -S
 
-# Enable color for pacman
 sed -i 's/^#Color/Color/' /etc/pacman.conf
-
 sed -i 's/^MAKEFLAGS.*/MAKEFLAGS="-j$(nproc)"/' /etc/makepkg.conf
 
 # Enable local hostname resolution
 systemctl enable avahi-daemon.service
-sed -i 's/resolve\ \[\!UNAVAIL\=return\]\ dns/mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] dns/' /etc/nsswitch.conf
+[ -z "$(grep mdns_minimal /etc/nsswitch.conf)" ] && sed -i 's/resolve\ \[\!UNAVAIL\=return\]\ dns/mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] dns/' /etc/nsswitch.conf
+
 systemctl enable cups.socket
 systemctl enable transmission
 
